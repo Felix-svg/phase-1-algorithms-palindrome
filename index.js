d220e8d79d50b45edb5eb67b57771fb6b73739f1
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-01-10 13:05:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reverse = word.split("").reverse().join("");
+  if (reverse === word) {
+    //console.log(true);
+    return true;
+  } else {
+    //console.log(false);
+    return false;
+  }
 }
+isPalindrome('mom')
 
 /* 
   Add your pseudocode here
+  if word is a palindrome, return true
+  else return false
 */
 
 /*
   Add written explanation of your solution here
+  Declared a variable then assigned the argument to it. I used the in-built
+  split, reverse and join methods. The split method splits the word, the
+  word is then reversed and joined
 */
 
 // You can run `node index.js` to view these console logs
